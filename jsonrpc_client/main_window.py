d99d8628b0533a027585f1308ddb67054e1c5f29
Roger Huang <roger.huang@amigo.com.tw>
2025-07-08 14:12:42 +0800

    main: refactory
    1. remove unused code
    2. correct for loop flow

--- jsonrpc_client/main_window.py
+++ jsonrpc_client/main_window.py
@@ -17,7 +17,7 @@ from jsonrpc_client.dlg.main_window import Ui_MainWindow
 import jsonrpc_client.dlg.resources # noqa: F401
 
 __author__ = "Roger Huang"
-__copyright__ = "Copyright 2024, The JSONRPC Client Project"
+__copyright__ = "Copyright 2025, The JSONRPC Client Project"
 __license__ = "Proprietary"
 __version__ = "2.0.1"
 __maintainer__ = "Roger Huang"
@@ -28,7 +28,6 @@ __status__ = "Package"
 class MainWindow(QMainWindow):
     def __init__(self, server_ip: str, port: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.counter = 0
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
@@ -54,19 +53,18 @@ class MainWindow(QMainWindow):
         worker = Worker(
             self.worker.run
         )  # Any other args, kwargs are passed to the run function
-        worker.signals.result.connect(self.print_output)
+        worker.signals.result.connect(self.print)
         worker.signals.finished.connect(self.thread_complete)
         worker.signals.progress.connect(self.progress_fn)
         # Execute
         self.threadpool.start(worker)
-
         self.timer = QTimer()
 
     def enable_fire(self):
         reg = self.ui.lineEdit_registerName.text()
         val = self.ui.lineEdit_value.text()
         self.ui.pushButton_fire.setEnabled(
-            True if reg and val else False)
+            True if len(reg) and len(val) else False)
 
     def wade(self):
         reg = self.ui.lineEdit_registerName.text()
@@ -79,7 +77,6 @@ class MainWindow(QMainWindow):
             "STR":"STR"
             }.get(sel)
         dir = self.ui.comboBox_directive.currentText()
-
         if type != "STR":
             val = int(val)
 
@@ -88,10 +85,11 @@ class MainWindow(QMainWindow):
         self.wade_model.layoutChanged.emit()
         self.ui.pushButton_fire.setEnabled(False)
         self.worker.write(json.dumps(self.last_wade))
-        self.timer.singleShot(2000, self.clear_wade)
+        self.timer.singleShot(1000, self.clear_wade)
 
     def clear_wade(self):
         self.last_wade = None
+        self.enable_fire()
 
     def wade_result(self, status: int, key: str, msg: str):
         if status == 2:
@@ -99,6 +97,7 @@ class MainWindow(QMainWindow):
             self.enable_fire()
         if self.last_wade:
             if self.last_wade.get(key):
+                # incase of timeout of wading
                 _, prev = self.wade_model.wades[-2]
                 prev[key] = msg
                 self.wade_model.wades[-2] = (status, prev)
@@ -120,6 +119,7 @@ class MainWindow(QMainWindow):
         if isinstance(msg, str):
             self.ui.statusbar.showMessage(msg)
             return
+
         if isinstance(msg, dict):
             handlers = {
                 'wade': self.wade_handler,
@@ -128,8 +128,9 @@ class MainWindow(QMainWindow):
                 handler = handlers.get(k)
                 if handler:
                     handler(v)
-                else:
-                    self.ui.plainText_eventView.appendPlainText(f'{k} : {v}')
+                    break
+            else:
+                self.ui.plainText_eventView.appendPlainText(f'{k} : {v}')
 
     def progress_fn(self, msg: str):
         handles = {
@@ -155,16 +156,9 @@ class MainWindow(QMainWindow):
             else:
                 self.ui.plainText_eventView.appendPlainText(msg)
 
-    def print_output(self, s):
-        print(s)
-
     def thread_complete(self):
         print("THREAD COMPLETE!")
         self.close()
-
-    def recurring_timer(self):
-        self.counter += 1
-        self.label.setText(f"Counter: {self.counter}")
 
     def closeEvent(self, event):
         self.worker.close()
